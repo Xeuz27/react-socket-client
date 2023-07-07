816ef26e2167aba481a9f584b754b34eb1afe722
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2023-07-07 12:01:08 -0400

    added state and funtion to create a new band within the submit of the form

--- src/components/bandAdd.js
+++ src/components/bandAdd.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const BandAdd = () => {
+const BandAdd = ({addBand}) => {
+  const [name, setName] = useState('')
+  const onSubmit = (e) => {
+    e.preventDefault()
+    if (name.trim().length > 0){
+      addBand( name ) //llamada al action creator para enviar la accion a
+    }
+    setName('')
+  }
+  
   return (
     <>
       <h3> agregar banda </h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
          className="" 
          placeholder="nombre de nueva banda"
+         value={name}
+         onChange={(e) =>setName(e.target.value)}
          />
       </form>
     </>
