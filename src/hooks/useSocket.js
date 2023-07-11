c71ac8220ff72d4aa89748689419ddc630fc1931
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2023-07-11 13:06:05 -0400

    created hook to instance a socket connection

---
+++ src/hooks/useSocket.js
@@ -0,0 +1,29 @@
+import { useMemo, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+export const useSocket = (serverPath) => {
+  const [online, setOnline] = useState(false);
+  const socket = 
+   useMemo(  () =>
+      io.connect(serverPath, {
+        transports: ["websocket"]
+      }),
+    [serverPath]
+  );
+  useEffect(() => {
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setOnline(true);
+    });
+  }, [socket]);
+  useEffect(() => {
+    socket.on("disconnect", () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
+  return { online, socket };
+};
