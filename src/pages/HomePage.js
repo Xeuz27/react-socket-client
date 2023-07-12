3fb71c26e827cad32adb45e0fdd75afdad5e9db4
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2023-07-12 17:25:20 -0400

    added some styling, and cleaned up the component from unnecessary logic, and moved that where is really needed and used

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,63 +1,42 @@
-import {useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 
 import "../App.css";
 import BandAdd from "../components/bandAdd";
 import BandList from "../components/bandList";
 import { SocketContext } from "../context/SocketContext";
 import { useSocket } from "../hooks/useSocket";
+import {BandChart} from "../components/BandChart";
 
 function HomePage() {
-   const {online, socket} = useContext(SocketContext)
-  //  const [bands, setBands] = useState([])
-  // const {online, socket} = useSocket('http://localhost:8080')
-
-
-  // useEffect(() => {
-  //   socket.on("current-bands", (bands) => {
-  //     setBands(bands)
-  //   });
-  // }, [socket]);
-
-  // const votar = (id) => {
-  //   socket.emit('votar-banda', id)
-  // }
-  // const borrarBanda = (id) => {
-  //   console.log('borrando banda', id)
-  //   socket.emit('borrar-banda', id)
-  // }
-  // const cambiarNombre = (id, nombre) => {
-  //   socket.emit('cambiar-nombre-banda', {id, nombre})
-  // }
-  // const addBand = (nombre) => {
-  //   socket.emit('add-band', nombre)
-  // }
-
-
-
+  const { online } = useContext(SocketContext);
   return (
     <div className="container">
       <div className="alert ml-8">
-        <p>
-          status:
+        <p className="font-medium">
+          Status:
           {online ? (
-            <span className="text-green-500"> online</span>
+            <span className="text-green-500 text-xl bg-green-100 p-1 rounded-lg">
+              Online
+            </span>
           ) : (
-            <span className="text-red-500"> offline</span>
+            <span className="text-red-500 text-xl bg-red-100 p-1 rounded-lg"> offline</span>
           )}
         </p>
       </div>
 
       <h1 className="w-content text-3xl ml-8">bandnames</h1>
       <hr />
+      <div className="w-[600px] mx-auto h-auto"><BandChart /></div>
+      
 
-      {/* <div className="flex ml-8">
+      <div className="flex ml-8">
         <div className="p-4 w-[60%] border border-solid border-sky-950">
-          <BandList data={bands} votar={votar} borrarBanda={borrarBanda} cambiarNombre={cambiarNombre} />
+          <BandList />
         </div>
         <div className="p-4 w-[40%] border-sky-950 border-solid border">
-          <BandAdd addBand={addBand} />
+          <BandAdd />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
